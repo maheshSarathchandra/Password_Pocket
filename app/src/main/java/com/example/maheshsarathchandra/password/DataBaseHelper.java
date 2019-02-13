f51fdb839de93828133ab22cb313e58533ccbcd7
maheshSarathchandra <msarathchandra190@gmail.com>
2019-02-13 21:54:13 +0530

    add items3

--- app/src/main/java/com/example/maheshsarathchandra/password/DataBaseHelper.java
+++ app/src/main/java/com/example/maheshsarathchandra/password/DataBaseHelper.java
@@ -5,76 +5,140 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
-public class DataBaseHelper extends SQLiteOpenHelper {
+import java.util.ArrayList;
+import java.util.List;
 
-    private static final String TAG = "DataBaseHelper";
-    private static final String TABLE_NAME = "customer_Table";
-    private static final String col ="id";
-    private static final String col1 = "password";
-    public DataBaseHelper(Context context){
-        super(context,TABLE_NAME,null,1);
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION = 1;
+
+    // Database Name
+    private static final String DATABASE_NAME = "notes_db";
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
+
+    // Creating Tables
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String createTable = "CREATE TABLE "+TABLE_NAME+"(ID INTEGER PRIMARY KEY AUTOINCREMENT,"+col1+"TEXT)";
-        db.execSQL(createTable);
+
+        // create notes table
+        db.execSQL(Note.CREATE_TABLE);
     }
 
+    // Upgrading database
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE EXISTS"+ TABLE_NAME);
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + Note.TABLE_NAME);
+
+        // Create tables again
         onCreate(db);
-
     }
-    public boolean addData(String item){
+
+    public long insertNote(String note) {
+        // get writable database as we want to write data
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
 
-        contentValues.put(col1,item);
-        Log.d(TAG,"addData: Adding"+item+"to"+TABLE_NAME);
-        long result = db.insert(TABLE_NAME,null,contentValues);
+        ContentValues values = new ContentValues();
+        // `id` and `timestamp` will be inserted automatically.
+        // no need to add them
+        values.put(Note.COLUMN_NOTE, note);
 
-        if(result == -1){
-            return false;
-        }else{
-            return true;
+        // insert row
+        long id = db.insert(Note.TABLE_NAME, null, values);
+
+        // close db connection
+        db.close();
+
+        // return newly inserted row id
+        return id;
+    }
+
+    public Note getNote(long id) {
+        // get readable database as we are not inserting anything
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(Note.TABLE_NAME,
+                new String[]{Note.COLUMN_ID, Note.COLUMN_NOTE, Note.COLUMN_TIMESTAMP},
+                Note.COLUMN_ID + "=?",
+                new String[]{String.valueOf(id)}, null, null, null, null);
+
+        if (cursor != null)
+            cursor.moveToFirst();
+
+        // prepare note object
+        Note note = new Note(
+                cursor.getInt(cursor.getColumnIndex(Note.COLUMN_ID)),
+                cursor.getString(cursor.getColumnIndex(Note.COLUMN_NOTE)),
+                cursor.getString(cursor.getColumnIndex(Note.COLUMN_TIMESTAMP)));
+
+        // close the db connection
+        cursor.close();
+
+        return note;
+    }
+
+    public List<Note> getAllNotes() {
+        List<Note> notes = new ArrayList<>();
+
+        // Select All Query
+        String selectQuery = "SELECT  * FROM " + Note.TABLE_NAME + " ORDER BY " +
+                Note.COLUMN_TIMESTAMP + " DESC";
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Note note = new Note();
+                note.setId(cursor.getInt(cursor.getColumnIndex(Note.COLUMN_ID)));
+                note.setNote(cursor.getString(cursor.getColumnIndex(Note.COLUMN_NOTE)));
+                note.setTimestamp(cursor.getString(cursor.getColumnIndex(Note.COLUMN_TIMESTAMP)));
+
+                notes.add(note);
+            } while (cursor.moveToNext());
         }
-    }
-    public Cursor getData(){
-       SQLiteDatabase db = this.getWritableDatabase();
-       String query = "SELECT * FROM "+ TABLE_NAME;
-       Cursor data = db.rawQuery(query,null);
-       return  data;
+
+        // close db connection
+        db.close();
+
+        // return notes list
+        return notes;
     }
 
-    public Cursor getItemID(String password){
+    public int getNotesCount() {
+        String countQuery = "SELECT  * FROM " + Note.TABLE_NAME;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(countQuery, null);
+
+        int count = cursor.getCount();
+        cursor.close();
+
+
+        // return count
+        return count;
+    }
+
+    public int updateNote(Note note) {
         SQLiteDatabase db = this.getWritableDatabase();
-        String query = "SELECT "+col+" FROM "+TABLE_NAME+
-        " WHERE "+col1+"='"+password+"'" ;
-        Cursor data = db.rawQuery(query,null);
-        return data;
+
+        ContentValues values = new ContentValues();
+        values.put(Note.COLUMN_NOTE, note.getNote());
+
+        // updating row
+        return db.update(Note.TABLE_NAME, values, Note.COLUMN_ID + " = ?",
+                new String[]{String.valueOf(note.getId())});
     }
 
-    public  void  updatePassward(String newPassword,int id, String oldPassword){
+    public void deleteNote(Note note) {
         SQLiteDatabase db = this.getWritableDatabase();
-        String query = "UPDATE "+TABLE_NAME+" SET "+col1+
-                "= '"+newPassword+"'WHERE"+col+"= '"+id+"'"+
-                "AND"+col1+"= '"+oldPassword+"'";
-
-        Log.d(TAG,"updatePassword: query: "+ query);
-        Log.d(TAG,"updatePassword: Setting password to"+ newPassword);
-
-        db.execSQL(query);
-
-    }
-    public void deletePassword(int id,String password){
-        SQLiteDatabase db = this.getWritableDatabase();
-        String query = "DELETE FROM "+TABLE_NAME+" WHERE "
-                +col+" = '"+id+"'"+"AND"+col1+" = '"+password+"'";
-        Log.d(TAG,"updatePassword: query: "+ query);
-        Log.d(TAG,"updatePassword: Setting password to"+ "from database");
-        db.execSQL(query);
+        db.delete(Note.TABLE_NAME, Note.COLUMN_ID + " = ?",
+                new String[]{String.valueOf(note.getId())});
+        db.close();
     }
 }
